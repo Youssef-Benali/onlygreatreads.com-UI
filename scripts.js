9712eaeb4ea47ae9522870eaa3198efb104a0924
Youssef <youssef.benali294@gmail.com>
2024-04-14 15:03:20 +0200

    Add dropdown menu

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// NavBar functions
+// NavBar UI close interaction for mobile menu
 function toggleNavigationBar() {
   let navbarMobileContainer = document.querySelector(
     ".navbar__mobile--container"
@@ -12,23 +12,24 @@ function closeMobileMenu() {
   document.querySelector(".navbar__mobile--container").style.display = "";
 }
 
-// Login status
-let isConnected = false;
+// Login UI interactions
+function logOut() {
+  let navbarLoginLinks = document.querySelector(".navbar__login");
+  let navbarConnectedOptions = document.querySelector(".navbar__rightgroup");
+  navbarLoginLinks.style.display = "flex";
+  navbarConnectedOptions.style.display = "none";
+}
 
 function logIn() {
   let navbarLoginLinks = document.querySelector(".navbar__login");
   let navbarConnectedOptions = document.querySelector(".navbar__rightgroup");
+  navbarLoginLinks.style.display = "none";
+  navbarConnectedOptions.style.display = "flex";
+}
 
-  isConnected = !isConnected;
+// Dropdown menu interaction
+function toggleDropDownMenu(){
+  let dropDownMenu = document.querySelector(".dropdown_menu")
 
-  if (isConnected) {
-    navbarLoginLinks.style.display = "none";
-    navbarConnectedOptions.style.display = "flex";
-  } else {
-    navbarLoginLinks.style.display = "flex";
-    navbarConnectedOptions.style.display = "none";
-  }
-
-  // console.log(navbarLoginLinks);
-  // console.log(navbarConnectedOptions);
+  dropDownMenu.style.display = dropDownMenu.style.display === "" ? "block" : ""
 }
