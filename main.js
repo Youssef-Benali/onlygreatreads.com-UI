07c6135d6e625337d16e854f07ede94070354e59
Youssef <youssef.benali294@gmail.com>
2024-05-30 17:11:32 +0200

    Add routing, Add routing for forms

---
+++ main.js
@@ -0,0 +1,24 @@
+// Basic Routing
+// js/main.js
+const routes = {
+  "/": "components/home.html",
+  "/loginForm": "components/loginForm.html",
+  "/registerForm": "components/registerForm.html",
+  "/contact": "contact.html",
+};
+
+const rootDiv = document.getElementById("content");
+
+const loadContent = async (path) => {
+  const response = await fetch(routes[path]);
+  const content = await response.text();
+  rootDiv.innerHTML = content;
+};
+
+const router = () => {
+  const path = window.location.hash.slice(1) || "/";
+  loadContent(path);
+};
+
+window.addEventListener("hashchange", router);
+window.addEventListener("load", router);
